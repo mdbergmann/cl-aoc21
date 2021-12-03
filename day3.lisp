8b58ea4cbf2c112af737a0eafe424593a616f881
Manfred Bergmann <mb@software-by-mabe.com>
2021-12-03 21:25:05 +0100

    day3-1

--- day3.lisp
+++ day3.lisp
@@ -1,8 +1,8 @@
 (in-package :cl-user)
-(ql:quickload '(:fiveam :str :binding-arrows))
+(ql:quickload '(:fiveam :str :binding-arrows :bit-smasher))
 
 (defpackage :aoc21.day3-test
-  (:use :cl :fiveam :str :binding-arrows)
+  (:use :cl :fiveam :str :binding-arrows :bit-smasher)
   (:export #:run!
            #:all-tests
            #:nil))
@@ -26,26 +26,80 @@
 ;;     (str:from-file)
 ;;     (parse-to-command-seq)))
 
-(defparameter *demo-input* '("00100"
-                             "11110"
-                             "10110"
-                             "10111"
-                             "10101"
-                             "01111"
-                             "00111"
-                             "11100"
-                             "10000"
-                             "11001"
-                             "00010"
-                             "01010"))
-
-(defun convert-string-to-bitvector (str)
+(defun string-to-bitvector (str)
   (coerce
    (mapcar (lambda (ch) (parse-integer (string ch))) (coerce str 'list))
    'bit-vector))
 
-(test convert-string-to-bitvector
-  (is (equalp #*00100 (convert-string-to-bitvector "00100"))))
+(defparameter *demo-input*
+  (mapcar #'string-to-bitvector
+          '("00100"
+            "11110"
+            "10110"
+            "10111"
+            "10101"
+            "01111"
+            "00111"
+            "11100"
+            "10000"
+            "11001"
+            "00010"
+            "01010")))
 
+(defun most-common-bit (bitvec-seq index)
+  (loop :for bitvec :in bitvec-seq
+        :for bit = (elt bitvec index)
+        :with ones = 0
+        :with zeroes = 0
+        :if (= 0 bit)
+          :do (incf zeroes)
+        :else
+          :do (incf ones)
+        :finally (return (if (> ones zeroes) 1 0))))
 
-(run! 'convert-string-to-bitvector)
+(defun most-common-bit-seq (bitvec-seq)
+  (coerce
+   (loop :for i :upto (- (length (car bitvec-seq)) 1)
+         :collect (most-common-bit bitvec-seq i))
+   'bit-vector))
+
+(defun least-common-bit-seq (bitvec-seq)
+  (bit-not (most-common-bit-seq bitvec-seq)))
+
+(test string-to-bitvector
+  (is (equalp #*00100 (string-to-bitvector "00100"))))
+
+(test most-common-bit-from-bitvector
+  (is (= 1 (most-common-bit *demo-input* 0)))
+  (is (= 0 (most-common-bit *demo-input* 1)))
+  (is (= 1 (most-common-bit *demo-input* 2)))
+  (is (= 1 (most-common-bit *demo-input* 3)))
+  (is (= 0 (most-common-bit *demo-input* 4))))
+
+(test most-common-bit-seq
+  (is (equalp #*10110 (most-common-bit-seq *demo-input*))))
+
+(test least-common-bit-seq
+  (is (equalp #*01001 (least-common-bit-seq *demo-input*))))
+
+(test day3-1-demo
+  (is (= 198 (* (int<- (most-common-bit-seq *demo-input*))
+                (int<- (least-common-bit-seq *demo-input*))))))
+
+(defparameter *input-1*
+  (->> #P"day3-input.txt"
+    (str:from-file)
+    (str:split #\NewLine)
+    (butlast)
+    (mapcar #'string-to-bitvector)))
+
+(test day3-1
+  (is (= 3429254 (* (int<- (most-common-bit-seq *input-1*))
+                    (int<- (least-common-bit-seq *input-1*))))))
+
+(run! 'string-to-bitvector)
+(run! 'most-common-bit-from-bitvector)
+(run! 'most-common-bit-seq)
+(run! 'least-common-bit-seq)
+(run! 'day3-1-demo)
+(run! 'day3-1)
