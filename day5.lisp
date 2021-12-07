19f6eb200dbdf7817d94a1cd9ee23ee19ae37614
Manfred Bergmann <mb@software-by-mabe.com>
2021-12-07 12:22:40 +0100

    Day5 reworked. much more efficient.

--- day5.lisp
+++ day5.lisp
@@ -56,23 +56,6 @@
 
 ;; -------------- demo --------------
 
-(defun operate-on-grid (grid-size collect-fun)
-  (loop :for x :to (car grid-size)
-        :append (loop :for y :to (cdr grid-size)
-                      :collect (funcall collect-fun (cons x y)))))
-
-(defun find-grid-size-from-lines (lines)
-  (loop :for line :in lines
-        :with max-x = 0
-        :with max-y = 0
-        :for line-x1 = (caar line)
-        :for line-x2 = (caadr line)
-        :for line-y1 = (cdar line)
-        :for line-y2 = (cdadr line)
-        :do (setf max-x (max max-x line-x1 line-x2)
-                  max-y (max max-y line-y1 line-y2))
-        :finally (return (cons max-x max-y))))
-
 (defun line-vertical-p (line)
   (let ((line-x1 (caar line))
         (line-x2 (caadr line)))
@@ -83,7 +66,18 @@
         (line-y2 (cdadr line)))
     (= line-y1 line-y2)))
 
-(defun filter-hor-or-vert-lines (lines)
+(defun line-diagonal-p (line)
+  (let* ((line-y1 (cdar line))
+         (line-y2 (cdadr line))
+         (line-x1 (caar line))
+         (line-x2 (caadr line))
+         (diff-x (- line-x2 line-x1))
+         (diff-y (- line-y2 line-y1)))
+    (or (= diff-x diff-y)
+        (= diff-x (- diff-y))
+        (= (- diff-x) diff-y))))
+
+(defun filter-hor-vert-lines (lines)
   (filter (lambda (line)
             (or (line-vertical-p line)
                 (line-horizontal-p line)))
@@ -94,23 +88,43 @@
     (setf (gethash point storage) (1+ curr))))
 
 (defun add-points-from-line (storage line)
-  (if (line-horizontal-p line)
-    (let* ((y (cdar line))
-           (x1 (caar line))
-           (x2 (caadr line))
-           (x-min (min x1 x2))
-           (x-max (max x1 x2)))
-      (loop :for x :from x-min :to x-max
-            :for point = (cons x y)
-            :do (add-point storage point)))
-    (let* ((x (caar line))
-           (y1 (cdar line))
-           (y2 (cdadr line))
-           (y-min (min y1 y2))
-           (y-max (max y1 y2)))
-      (loop :for y :from y-min :to y-max
-            :for point = (cons x y)
-            :do (add-point storage point)))))
+  (let* ((y1 (cdar line))
+         (y2 (cdadr line))
+         (x1 (caar line))
+         (x2 (caadr line))
+         (x-min (min x1 x2))
+         (x-max (max x1 x2))
+         (y-min (min y1 y2))
+         (y-max (max y1 y2)))
+    (cond
+      ((line-horizontal-p line)
+       (loop :for x :from x-min :to x-max
+             :for point = (cons x y1)
+             :do (add-point storage point)))
+      ((line-vertical-p line)
+       (loop :for y :from y-min :to y-max
+             :for point = (cons x1 y)
+             :do (add-point storage point)))
+      ((line-diagonal-p line)
+       (if (and (>= x2 x1)
+                (>= y2 y1))
+           (loop :for x :from x-min :to x-max
+                 :for y :from y-min :to y-max
+                 :for point = (cons x y)
+                 :do (add-point storage point))
+           (loop :for x :from x-max :downto x-min
+                 :for y :from y-min :to y-max
+                 :for point = (cons x y)
+                 :do (add-point storage point)))))))
+
+(defun collect-overlaps (n storage)
+  (let ((result 0))
+    (maphash (lambda (k v)
+               (declare (ignore k))
+               (if (>= v n)
+                   (incf result)))
+             storage)
+    result))
 
 (test is-line-vertical
   (is-true (line-vertical-p '((0 . 0) (0 . 5))))
@@ -123,7 +137,7 @@
 (test filter-lines-for-hor-or-vert-only
   (let ((lines (parse-to-lines-from-string *demo-input*)))
     (is (= 10 (length lines)))
-    (is (= 6 (length (filter-hor-or-vert-lines lines))))))
+    (is (= 6 (length (filter-hor-vert-lines lines))))))
 
 (test build-storage-from-line-points
   (let ((storage (make-hash-table :test #'equal :lock-free t)))
@@ -138,38 +152,81 @@
     (is (= 1 (gethash '(0 . 2) storage)))
     (is-false (gethash '(1 . 2) storage))))
 
+(test build-storage-from-diag-line-points
+  (let ((storage (make-hash-table :test #'equal :lock-free t)))
+    (add-points-from-line storage '((1 . 1) (3 . 3)))
+    (is (= 1 (gethash '(1 . 1) storage)))
+    (is (= 1 (gethash '(2 . 2) storage)))
+    (is (= 1 (gethash '(3 . 3) storage)))
+    (add-points-from-line storage '((9 . 7) (7 . 9)))
+    (is (= 1 (gethash '(9 . 7) storage)))
+    (is (= 1 (gethash '(8 . 8) storage)))
+    (is (= 1 (gethash '(7 . 9) storage)))))
+
+;; --------------- 1 demo ----------------
+
 (test day5-demo
-  (let ((lines (filter-hor-or-vert-lines
+  (let ((lines (filter-hor-vert-lines
                 (parse-to-lines-from-string *demo-input*)))
-        (storage (make-hash-table :test #'equal :lock-free t))
-        (result 0))
+        (storage (make-hash-table :test #'equal :lock-free t)))
     (dolist (line lines)
       (add-points-from-line storage line))
-    (print storage)
-    (maphash (lambda (k v) (if (>= v 2)
-                          (incf result)))
-             storage)
-    (is (= 5 result))))
+    (is (= 5 (collect-overlaps 2 storage)))))
 
 (run! 'is-line-vertical)
 (run! 'is-line-horizontal)
 (run! 'filter-lines-for-hor-or-vert-only)
 (run! 'build-storage-from-line-points)
+(run! 'build-storage-from-diag-line-points)
 (run! 'day5-demo)
 
 ;; --------------- 1 ----------------
 
 (test day5-1
-  (let ((lines (filter-hor-or-vert-lines
+  (let ((lines (filter-hor-vert-lines
                 (parse-to-lines-from-string (str:from-file #P"day5-input.txt"))))
-        (storage (make-hash-table :test #'equal :lock-free t))
-        (result 0))
+        (storage (make-hash-table :test #'equal :lock-free t)))
     (dolist (line lines)
       (add-points-from-line storage line))
-    (print storage)
-    (maphash (lambda (k v) (if (>= v 2)
-                          (incf result)))
-             storage)
-    (is (= 5092 result))))
+    (is (= 5092 (collect-overlaps 2 storage)))))
 
 (run! 'day5-1)
+
+;; --------------- 2 demo -------------
+
+(defun filter-hor-vert-diag-lines (lines)
+  (filter (lambda (line)
+            (or (line-vertical-p line)
+                (line-horizontal-p line)
+                (line-diagonal-p line)))
+          lines))
+
+(defun day5-2-demo ()
+  (let ((lines (filter-hor-vert-diag-lines
+                (parse-to-lines-from-string *demo-input*)))
+        (storage (make-hash-table :test #'equal :lock-free t)))
+    (dolist (line lines)
+      (add-points-from-line storage line))
+    (collect-overlaps 2 storage)))
+
+(test diag-line
+  (is-true (line-diagonal-p '((1 . 1) (3 . 3))))
+  (is-true (line-diagonal-p '((9 . 7) (7 . 9)))))
+
+(test day5-2-demo
+    (is (= 12 (day5-2-demo))))
+
+(run! 'diag-line)
+(run! 'day5-2-demo)
+
+;; ---------------- 2 -----------------
+
+(test day5-2
+  (let ((lines (filter-hor-vert-diag-lines
+                (parse-to-lines-from-string (str:from-file #P"day5-input.txt"))))
+        (storage (make-hash-table :test #'equal :lock-free t)))
+    (dolist (line lines)
+      (add-points-from-line storage line))
+    (is (= 22066 (collect-overlaps 2 storage)))))
+
+(run! 'day5-2)
